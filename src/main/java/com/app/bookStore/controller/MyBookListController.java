45b0bb78e7eaf9e5ad95287b965793a33d10c630
Mohmmad <mdzaheerkr@gmail.com>
2026-09-16 10:33:20 +0530

    upload project

---
+++ src/main/java/com/app/bookStore/controller/MyBookListController.java
@@ -0,0 +1,24 @@
+package com.app.bookStore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.app.bookStore.service.MyBookListService;
+
+@Controller
+public class MyBookListController {
+	@Autowired
+	private MyBookListService service;
+	
+	@RequestMapping("/deleteMyList/{id}")
+	
+	
+	public String deleteMyList(@PathVariable("id")int id ) {
+		service.deleteById(id);
+	
+		return"redirect:/my_books";
+	}
+
+}
---
+++ src/main/java/com/app/bookStore/service/MyBookListService.java
@@ -0,0 +1,22 @@
+package com.app.bookStore.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.bookStore.entity.MyBookList;
+import com.app.bookStore.repository.MyBookRepository;
+@Service
+public class MyBookListService {
+	@Autowired
+private MyBookRepository mybook;
+public void saveMyBooks(MyBookList book) {
+	mybook.save(book);
+}  
+public List<MyBookList> getAllMyBooks(){
+	return mybook.findAll();
+}
+public void deleteById(int id) {
+	mybook.deleteById(id);
+}
+}
